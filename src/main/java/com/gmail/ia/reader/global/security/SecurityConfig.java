6a9d8c6aff62b0fe157cbd7fbb99c17f2408274c
CHRISTIAN1409X <christianfelippe1409@gmail.com>
2026-06-12 11:50:52 -0500

    ip-access

--- src/main/java/com/gmail/ia/reader/global/security/SecurityConfig.java
+++ src/main/java/com/gmail/ia/reader/global/security/SecurityConfig.java
@@ -88,6 +88,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(Arrays.asList(
+                "http://127.0.0.1:5173",
                 "http://localhost:5173",
                 "http://localhost:4173"
            //     String.format("http://%s", myLocalIp),
